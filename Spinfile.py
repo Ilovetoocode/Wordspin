ef1f4acea639008b102de7344d5496fde090cded
LongNguyen567 <fairplayguy12345@gmail.com>
2024-02-13 15:14:36 -0700

    add a read_text method and remove_punctuation method but still working on that one

--- Spinfile.py
+++ Spinfile.py
@@ -2,8 +2,20 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import Spinner from Spinner
+
+from Spinner import Spinner
+import string
+
+def remove_punctuation(text):
+
+def read_text(filename):
+   with open(filename,'r') as file:
+      text = file.read().lower()
+      text = remove_punctuation(text)
+      return text
+
 def main():
+
 
 
 # Press the green button in the gutter to run the script.
