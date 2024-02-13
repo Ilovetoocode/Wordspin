1585233b879942b838cd69afd44ec0a52f805936
Connor Walton <46007308+Ilovetoocode@users.noreply.github.com>
2024-02-13 12:52:42 -0700

    Grabbed this code from the chatbot, makes a list from a file

--- Spinfile.py
+++ Spinfile.py
@@ -2,10 +2,10 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import Spinner
 
 def main():
-
+   Spinner.make_list_from_file("synonyms-simplified.txt")
 
 
 # Press the green button in the gutter to run the script.
--- Spinner.py
+++ Spinner.py
@@ -1,1 +1,7 @@
 import random
+def make_list_from_file(file):
+    with open(file, 'r', encoding='utf8') as file:
+        items = []
+        for line in file:
+            items += line.splitlines()
+        print(items)
