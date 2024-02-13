4e9f52de05ce18e7cc78db703fa4f1ac8ddb9675
Connor Walton <46007308+Ilovetoocode@users.noreply.github.com>
2024-02-13 14:36:43 -0700

    Right this didn't break anything, so I'm fixing this again

--- Spinfile.py
+++ Spinfile.py
@@ -2,8 +2,9 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import Spinner
+import Spinner from Spinner
 def main():
+   
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
