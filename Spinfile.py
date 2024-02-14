92e34f731a81c9956ca668db65f310f793bdb1d9
Connor Walton <46007308+Ilovetoocode@users.noreply.github.com>
2024-02-14 16:36:49 -0700

    This writing function works now

--- Spinfile.py
+++ Spinfile.py
@@ -23,8 +23,9 @@ def main():
    spinner = Spinner(original_text)
    for i in range(3): # convert the original text 3 times
       changed_text = spinner.Spintowin()
-      result_file.write("Option {i+1}\n")
-      result_file.write(changed_text + "\n\n")
+      with open('results.txt', 'a') as result_file:
+         result_file.write("Option {i+1}\n")
+         result_file.write(changed_text + "\n\n")
       original_text = changed_text
       print("Option", i+1, "\n", changed_text) # prints the index of the current location
 
