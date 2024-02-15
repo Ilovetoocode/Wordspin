abaa370d7ecd19855ede624c53038a2852f6011d
Connor Walton <46007308+Ilovetoocode@users.noreply.github.com>
2024-02-15 09:36:39 -0700

    Readability update! Following things changed: Added lots of comments. Made new functions to de-clutter main Fixed some older typos General readability improvements. Should be good to submit!

--- Spinfile.py
+++ Spinfile.py
@@ -1,36 +1,57 @@
-# This is a sample Python script.
+# Code by Connor Walton and Long Nyguen
+# Assignment A4, Word Spinner for comp 1020
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# Starting out: we import both the spinner object and the string package.
 from Spinner import Spinner
 import string
-import sys
 
+
+# This function is used to grab a text file and use it as a string value.
+# This is done so later on we can use it on the essay.txt file as a parameter for the Spinner object.
 def read_text(filename):
-   with open(filename,'r') as file:
-      text = file.read().lower().translate(str.maketrans('', '', string.punctuation))
-      return text
+    with open(filename, 'r') as file:
+        text = file.read().lower().translate(str.maketrans('', '', string.punctuation))
+        return text
 
+
+# This function's a little bit extra! The original idea goes to Nyguen. He decided that the results file should
+# automagically be updated each time you ran the program! The original version had this code in the main function,
+# as seen in the git repo For readability however, anything pertaining to the printing has been turned into its own
+# function to be called. With all that out of the way, this first one overwrites the original results.txt file,
+# starting it over with the original text. This makes sure that the file is cleaned up, making the next step of the
+# output file work more easily. Note however this will destroy any previous version of the results file,
+# so if there's any results you like in a generation? Make sure you save them somewhere else before running the
+# program again!
+def file_writer(text_to_print):
+    with open('results.txt', 'w') as result_file:
+        result_file.write("Original\n")
+        result_file.write(text_to_print + "\n\n")
+
+
+# This function takes in the same results file as the file writer above.
+# The only difference? This function is set to append mode, rather than write mode.
+# This results in this function being able to add new data to a file, rather than fully overwriting one.
+def file_appender(text_to_add):
+    with open('results.txt', 'a') as result_file:
+        result_file.write("Option {i+1}\n")
+        result_file.write(text_to_add + "\n\n")
+
+
+# #Finally we reach the main function. #This function: 1: calls the read_text function to get the essay for the
+# original_text variable, #2: Prints that text out with a marker showing it is the original version #3:Calls the file
+# writer to overwrite the results file with that text #4: Creates a Spinner object named spinner using the
+# original_text as the argument #5:Creates a loop that uses the spintowin method to change the text, and using that
+# result to print to console and pass into the file appender.
 def main():
-   original_text = read_text('essay.txt') # read the original text from essay
-   print("Original", "\n", original_text)
+    original_text = read_text('essay.txt')  # read the original text from essay
+    print("Original", "\n", original_text)
+    file_writer(original_text)
+    spinner = Spinner(original_text)
+    for i in range(3):  # convert the original text 3 times
+        changed_text = spinner.spintowin()
+        file_appender(changed_text)
+        print("Option", i + 1, "\n", changed_text)  # prints the index of the current location
 
-   with open('results.txt', 'w') as result_file:
-      result_file.write("Original\n")
-      result_file.write(original_text + "\n\n")
 
-   spinner = Spinner(original_text)
-   for i in range(3): # convert the original text 3 times
-      changed_text = spinner.Spintowin()
-      with open('results.txt', 'a') as result_file:
-         result_file.write("Option {i+1}\n")
-         result_file.write(changed_text + "\n\n")
-      original_text = changed_text
-      print("Option", i+1, "\n", changed_text) # prints the index of the current location
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-   main();
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
--- Spinner.py
+++ Spinner.py
@@ -1,58 +1,61 @@
 import random
-## Code from the bot project that was needed here! Simply makes a list from the file.... Infact...
+
+
+# Code from the bot project that was needed here! Simply makes a list from the file.... In fact...
 def make_list_from_file(file):
     with open(file, 'r', encoding='utf8') as file:
         items = []
         for line in file:
             items += line.splitlines()
         return items
-## This second one is also from the bot project! Though, it's been, slightly modified... Originally it would only have one list
-##For broken list stuff, but due to how this new program needs to handle abritrary numbers of values for keys. Works pretty well!
-def make_synonym_dictionary(list):
+
+
+# This second one is also from the bot project! Though, it's been, slightly modified... Originally it would only have
+# one list For broken list stuff, but due to how this new program needs to handle arbitrary numbers of values for
+# keys. Works pretty well!
+def make_synonym_dictionary(synlist):
     synonymdict = {}
-    broken_list = []
-    second_broken_list = []
-    for lines in list:
-        broken_list=lines.split(":")
-        synonymdict[broken_list[0]]=[]
-        second_broken_list=broken_list[1].split(",")
-        synonymdict[broken_list[0]]+=second_broken_list
+    for lines in synlist:
+        broken_list = lines.split(":")
+        synonymdict[broken_list[0]] = []
+        second_broken_list = broken_list[1].split(",")
+        synonymdict[broken_list[0]] += second_broken_list
     return synonymdict
-##Here's a function that, pretty much exists so I don't have to nest a later method as much.... Pulls input text and randomizes it from a given dictionary for synonyms.
-##Really all of this code didn't need to be up here... I just did it like this so I could minimize the clutter down in the Spintowin method.
-##Works very well this way, and also means you need not pay as much attention to... nonsense nested code clutter
-def wordrandomizer(input, syndict):
-    maybe_change=random.randint(0,100)
-    new_word=""
+
+
+# Here's a function that, pretty much exists, so I don't have to nest a later method as much.... Pulls input text and
+# randomizes it from a given dictionary for synonyms. Really all of this code didn't need to be up here... I just did
+# it like this, so I could minimize the clutter down in the Spintowin method. Works very well this way, and also means
+# you need not pay as much attention to... nonsense nested code clutter
+def wordrandomizer(input_word, syndict):
+    maybe_change = random.randint(0, 100)
     if maybe_change >= 10:
-        word_change=random.randint(0,len(syndict[input])-1)
-        new_word=syndict[input][word_change]
+        word_change = random.randint(0, len(syndict[input_word]) - 1)
+        new_word = syndict[input_word][word_change]
     else:
-        new_word=input
+        new_word = input_word
     return new_word
 
 
 class Spinner:
-    ##The spinner itself, first part calls earlier functions for the dictionary, alongside taking an input agrument
-    ##so it can work with the one method it has.
-    def __init__ (self, input):
-        self.synonyms= make_synonym_dictionary(make_list_from_file("synonyms-simplified.txt"))
-        self.input=input.split()
-    def Spintowin(self):
-        ##This is the main spinning function itself, taking in text to determine whether or not to send it to the wordrandomizer function defined earlier, putting in either the output of the function
-        ##Or just putting in the text itself if the word's not in the self.synonyms dictionary
-        ##Here you can see why I moved that up, probably. The if word in part would have, looked so much worse thanks to the extra nesting needed....
+    # The spinner itself, first part calls earlier functions for the dictionary, alongside taking an input agrument,
+    # so it can work with the one method it has.
+    def __init__(self, word_input):
+        self.synonyms = make_synonym_dictionary(make_list_from_file("synonyms-simplified.txt"))
+        self.input = word_input.split()
 
-        output=""
-        input=self.input
-        for word in input:
+    def spintowin(self):
+        # This is the main spinning function itself, taking in text to determine whether to send it to the
+        # wordrandomizer function defined earlier, putting in either the output of the function Or just putting in
+        # the text itself if the word's not in the self.synonyms dictionary Here you can see why I moved that up,
+        # probably. The if word in part would have, looked so much worse thanks to the extra nesting needed....
+
+        output = ""
+        input_word = self.input
+        for word in input_word:
             if word in self.synonyms:
-                output+=(wordrandomizer(word, self.synonyms))
+                output += (wordrandomizer(word, self.synonyms))
             else:
-                output+=(word)
-            output+=" "
+                output += word
+            output += " "
         return output
-
-
-
-
